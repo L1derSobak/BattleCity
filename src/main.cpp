5a1e2b2dd0ddf4247543d5a82e902d8186bc3c05
Sergey <veryluckyportal4all@gmail.com>
2021-08-14 13:39:42 +0300

    OpenGL and glfw initilization

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,29 @@
 
 #include <iostream>
 
+GLint g_WindowSizeX = 640;
+GLint g_WindowSizeY = 480;
+void glfwWindowSizeCallback(GLFWwindow* pWindow, GLint width, GLint height);
+void glfwKeyCallback(GLFWwindow* pWindow, GLint key, GLint scancode, GLint action, GLint mode);
+
 int main(void)
 {
     GLFWwindow* window;
-
+    
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(g_WindowSizeX, g_WindowSizeY, "Hello World", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, &glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -27,9 +35,9 @@ int main(void)
 		std::cout<<"Can't load GLAD!"<<std::endl;
 		return -1;
 	}
-	
-    //std::cout << "OpenGL " << GLVersion.major << "." << GLVerison.minor << std::endl;
-    	
+    std::cout << "Render: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
 	glClearColor(1,1,0,1);
 	
     /* Loop until the user closes the window */
@@ -47,4 +55,20 @@ int main(void)
 
     glfwTerminate();
     return 0;
+}
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, GLint width, GLint height)
+{
+    g_WindowSizeX = width;
+    g_WindowSizeY = height;
+    glViewport(0, 0, g_WindowSizeX, g_WindowSizeY);
+    std::cout << "IT CHANGED!!!!" << std::endl;
+}
+
+void glfwKeyCallback(GLFWwindow* pWindow, GLint key, GLint scancode, GLint action, GLint mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
 }
